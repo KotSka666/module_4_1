392b9a218900d2945a3f12e0fcbbaf6cbf7e77fa
Konstantin <90984821+KotSka666@users.noreply.github.com>
2024-10-30 08:58:31 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,6 @@
+def divide(first, second):
+    if second == 0:
+        return f'Ошибка'
+    else:
+        res = first / second
+        return res
---
+++ module_4_1.py
@@ -0,0 +1,7 @@
+import true_math as tm
+from fake_math import divide as fm
+
+print(tm.divide(5, 0))
+print(tm.divide(69, 3))
+print(fm(6, 0))
+print(fm(49, 7))
